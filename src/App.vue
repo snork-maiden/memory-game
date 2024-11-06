5a87a12b322ac9b2d4d7f399be3e3f6b6c726fe9
snork-maiden <pollykoldysheva@gmail.com>
2024-11-06 14:26:19 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TheMemoryGame from "./components/TheMemoryGame.vue";
+</script>
+
+<template>
+  <main class="main">
+    <h1 class="title">Memory match</h1>
+    <TheMemoryGame />
+  </main>
+</template>
+
+<style scoped>
+ .main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+ }
+</style>
---
+++ src/components/TheMemoryGame.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MemoryCard from "./MemoryCard.vue";
+import ReplayButton from "./ReplayButton.vue";
+</script>
+
+<template>
+  <ReplayButton />
+  <div class="wrapper">
+    <MemoryCard />
+  </div>
+</template>
